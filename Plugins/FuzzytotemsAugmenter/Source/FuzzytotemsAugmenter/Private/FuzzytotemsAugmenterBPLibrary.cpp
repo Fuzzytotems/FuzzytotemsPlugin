5556bb5ec1ed619dc4dd0dc8a03be46ef280d33a
Psyodronix <e.s.fisher@psyodronix.com>
2015-11-21 22:22:30 -0500

    Version 0.1 RC 1
    - Plugin source added
    - TArray blueprint functionality added

---
+++ Plugins/FuzzytotemsAugmenter/Source/FuzzytotemsAugmenter/Private/FuzzytotemsAugmenterBPLibrary.cpp
@@ -0,0 +1,43 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+#include "FuzzytotemsAugmenterPrivatePCH.h"
+#include "FuzzytotemsAugmenterBPLibrary.h"
+
+UFuzzytotemsAugmenterBPLibrary::UFuzzytotemsAugmenterBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+bool UFuzzytotemsAugmenterBPLibrary::TArraySortFloat(TArray<float> input, TArray<float> &output, ESortType setting = ESortType::EST_Sort)
+{
+	return InternalPrimitiveSort<float>(input, output, setting);
+}
+
+bool UFuzzytotemsAugmenterBPLibrary::TArraySortInt(TArray<int32> input, TArray<int32> &output, ESortType setting = ESortType::EST_Sort)
+{
+	InternalPrimitiveSort<int32>(input, output, setting);
+	return true;
+}
+
+template<typename t>
+bool UFuzzytotemsAugmenterBPLibrary::InternalPrimitiveSort(TArray<t> input, TArray<t> &output, ESortType setting)
+{
+	switch (setting)
+	{
+	case ESortType::EST_UnstableSort:
+		input.Sort();
+		break;
+	case ESortType::EST_HeapSort:
+		input.HeapSort();
+		break;
+	case ESortType::EST_Sort:
+	default:
+		input.StableSort();
+		break;
+	}
+
+	output = input;
+
+	return true;
+}
